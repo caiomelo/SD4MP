cba2252074bb469962b79da048af4a6436f2a92c
Caio Melo <caio.compufc@gmail.com>
2015-04-13 17:28:46 -0300

    Started service project final version.

---
+++ StartedServiceLifeCycle/app/src/main/java/br/ufc/dc/sd4mp/startedservicelifecycle/MyService.java
@@ -0,0 +1,64 @@
+package br.ufc.dc.sd4mp.startedservicelifecycle;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyService extends Service implements  Runnable {
+
+    private static final String LOG_CATEGORY = "MyServiceLogCat";
+
+    private boolean running = false;
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(LOG_CATEGORY, getClassName() + "onCreate");
+    }
+
+    @Override
+    public  void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_CATEGORY, getClassName() + "onDestroy");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(LOG_CATEGORY, getClassName() + "onStartCommand");
+        running = true;
+        run();
+        return START_NOT_STICKY;
+    }
+
+    private String getClassName() {
+        return MyService.class.getName();
+    }
+
+    public void run() {
+        int i = 0;
+
+        while (running && i<5) {
+            String message = "MyService is running!";
+            Log.i("(" + i++ +") MyService", message);
+
+            try {
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+
+            }
+        }
+
+        running = false;
+    }
+}
